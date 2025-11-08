33c4b8b58b422893a43a8a3067ba493c4da33ff2
iyyneswaran <pdee5660@gmail.com>
2025-11-08 15:23:56 +0530

    route for home

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<Landingpage />} /> */}
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/analysis" element={<Analysis />} />
         <Route path="/weather" element={<Weather />} />
