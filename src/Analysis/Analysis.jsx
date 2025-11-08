61a6cd3262e0fc5b02c6ca741b37641571591202
iyyneswaran <pdee5660@gmail.com>
2025-11-08 18:42:44 +0530

    testing

--- src/Analysis/Analysis.jsx
+++ src/Analysis/Analysis.jsx
@@ -5,7 +5,7 @@ import { BsStars } from "react-icons/bs";
 import { FiUploadCloud, FiInfo, FiX, FiCheck, FiLoader } from "react-icons/fi";
 import Navbar from "../nav/Navbar";
 
-const BACKEND_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BACKEND_URL = import.meta.env.VITE_API_URL;
 
 export default function Analysis() {
   const fileInputRef = useRef(null);
